550c832e36fcb35ba56d8b0f0155125cbdf8bff4
Elham Badri <48000928+ElhamBadri2411@users.noreply.github.com>
2022-05-20 13:57:35 -0400

    Time: 56 ms (20.72%), Space: 14 MB (24.54%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -3,13 +3,14 @@ class Solution:
         stack = []
         
         for c in s:          
-            if c == '(' or c == '{' or c =='[':
+            if c == '(' or c == '{' or c =='[': # if its an opening bracket we add it to the stacj
                 stack.append(c)
             else:
-                if not stack:
+                if not stack: # if the stack is empty at this point and we have a closing bracket, its not correct
                     return False
                 
-                if c == ')' and stack[-1] == '(':
+                # check the stack if closing bracket corresponds with opening
+                if c == ')' and stack[-1] == '(': 
                     stack.pop()
                 elif c == '}' and stack[-1] == '{':
                     stack.pop()
@@ -18,8 +19,9 @@ class Solution:
                 else:
                     return False
                 
-        if len(stack) == 0:        
+        if len(stack) == 0:   # check if the stack is empty     
             return True 
+        
         return False        
                     
                 
