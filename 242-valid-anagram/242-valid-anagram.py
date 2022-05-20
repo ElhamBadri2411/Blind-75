69c484677c2380c8c104ae0aa4335e86d94b4168
Elham Badri <48000928+ElhamBadri2411@users.noreply.github.com>
2022-05-20 13:24:12 -0400

    Time: 117 ms (6.84%), Space: 14.5 MB (35.08%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        lookup_s = {}
+        lookup_t = {}
+        
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            lookup_s[s[i]] = lookup_s.get(s[i], 0) + 1
+            lookup_t[t[i]] = lookup_t.get(t[i], 0) + 1
+            
+        for letter in lookup_s:
+            if letter not in lookup_t or lookup_s[letter] != lookup_t[letter]:
+                return False
+        
+        
+        
+        return True
