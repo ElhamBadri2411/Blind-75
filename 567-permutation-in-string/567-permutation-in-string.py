5032097429e69a238407bdfa51d38a7c793e55a7
Elham Badri <48000928+ElhamBadri2411@users.noreply.github.com>
2022-06-22 12:49:42 -0400

    Time: 73 ms (91.06%), Space: 14.1 MB (30.78%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,27 +4,27 @@ class Solution:
         matches = 0
         lookup = {}
         
-        for letter in s1:
+        #Add all the letters in the s1 string to a hashmap
+        for letter in s1: 
             if letter not in lookup:
                 lookup[letter] = 0
             lookup[letter] += 1
             
-            
-            
+        #Incrementing the window            
         for window_end in range(len(s2)):
             r_char = s2[window_end]
             
-            if r_char in lookup:
-                lookup[r_char] -= 1
-                if lookup[r_char] == 0:
+            if r_char in lookup: # if the character is in the s1 lookup
+                lookup[r_char] -= 1 # we reduce the count of the letter
+                if lookup[r_char] == 0: # if the lookup hits 0, increase the number of matches by one
                     matches += 1
                 
-            if matches == len(lookup):
+            if matches == len(lookup): # check if the matches is equal to the number of unique numbers
                 return True
             
             
             
-            if window_end >= len(s1) - 1:
+            if window_end >= len(s1) - 1: #if the window_end index is larger than the max len, we decrement the start 
                 l_char = s2[window_start]
                 window_start += 1
                 
